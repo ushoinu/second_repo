e48424361f796f81e57e8716f78bf84539364aeb
Ushoinu Marma <ushoinumarma@gmail.com>
2024-04-22 17:14:47 -0700

    first commit

--- miniGame.java
+++ miniGame.java
@@ -1,8 +1,12 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class miniGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        Random ran = new Random();
+        int n = scan.nextInt();
+        int a = ran.nextInt(0,11);
+
     }
 }
