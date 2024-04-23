a2019e6ec479b0bbc48156345a3b538a19ec5f53
Ushoinu Marma <ushoinumarma@gmail.com>
2024-04-22 17:23:28 -0700

    add output

--- miniGame.java
+++ miniGame.java
@@ -5,8 +5,16 @@ public class miniGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random ran = new Random();
-        int n = scan.nextInt();
-        int a = ran.nextInt(0,11);
-
+        int a = ran.nextInt(0, 11);
+        int b;
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Please enter a number: ");
+            b = scan.nextInt();
+            if (a!=b) {
+                System.out.println("You have "+(3-i)+"change's");
+            }else{
+                System.out.println("you win");
+            }
+        }
     }
 }
